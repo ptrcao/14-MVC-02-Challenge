160b1cb89c9e4274fd3db5d7827bb6837d81b5f2
ptrcao <ptrcao@gmail.com>
2023-03-17 02:31:52 +1100

    Dynamic sequelize connection

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,24 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 // require('dotenv').config({ path: '../.env' });
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: process.env.PORT,
+
+
+if (process.env.JAWSDB_URL) {
+    var sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    var sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+          host: 'localhost',
+          dialect: 'mysql',
+          port: process.env.PORT,
+        }
+      );
   }
-);
+
+
 
 module.exports = sequelize;
 
