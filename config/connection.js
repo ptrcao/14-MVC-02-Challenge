801f54487cf782106fa3570430c365201a104681
ptrcao <ptrcao@gmail.com>
2023-03-17 04:20:06 +1100

    Fix db connection.js for Heroku

--- config/connection.js
+++ config/connection.js
@@ -3,22 +3,22 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 // require('dotenv').config({ path: '../.env' });
 
-var sequelize;
+let sequelize;
 
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-  } else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-          host: 'localhost',
-          dialect: 'mysql',
-          port: process.env.SERVER_PORT,
-        }
-      );
-  }
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
+}
 
 
 
